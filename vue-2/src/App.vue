f1402a03b7e78351026101585b22a85792ad40f6
nasa <2974464448@qq.com>
2023-05-25 16:34:15 +0800

    2222

--- vue-2/src/App.vue
+++ vue-2/src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+   <ZangFei />
 </template>
+
+
+<script>
+import ZangFei from './views/ZangFei.vue'
+export default {
+  components:{
+    ZangFei,
+  }
+}
+</script>
 
 <style lang="less">
 #app {
@@ -14,19 +18,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
